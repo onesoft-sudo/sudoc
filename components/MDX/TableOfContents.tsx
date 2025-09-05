363b8678be3f94eb676765268d3318f792ea0343
Ar Rakin <rakinar2@osndevs.org>
2025-09-05 23:10:56 +0600

    fix: toc auto scrolling
    Signed-off-by: Ar Rakin <rakinar2@osndevs.org>

--- components/MDX/TableOfContents.tsx
+++ components/MDX/TableOfContents.tsx
@@ -1,106 +1,119 @@
 "use client";
 
 import useActualPathname from "@/hooks/useActualPathname";
-import { FC, useEffect, useRef, useState, JSX } from "react";
+import {
+    FC,
+    JSX,
+    useEffect,
+    useRef,
+    useState
+} from "react";
 
 const selector = ":is(h1, h2, h3, h4, h5, h6)[id]";
 
 export default function TableOfContents({
-    as,
+	as,
 }: {
-    as?: keyof JSX.IntrinsicElements | FC;
+	as?: keyof JSX.IntrinsicElements | FC;
 }) {
-    const [headings, setHeadings] = useState<
-        {
-            id: string;
-            title: string;
-        }[]
-    >([]);
-    const observer = useRef<IntersectionObserver>(null);
-    const [activeId, setActiveId] = useState("");
-    const Root = as ?? "div";
-    const pathname = useActualPathname();
+	const [headings, setHeadings] = useState<
+		{
+			id: string;
+			title: string;
+		}[]
+	>([]);
+	const observer = useRef<IntersectionObserver>(null);
+	const [activeId, setActiveId] = useState("");
+	const Root = as ?? "div";
+	const pathname = useActualPathname();
 
-    useEffect(() => {
-        const headingElements = Array.from(
-            document.querySelectorAll(selector) as Iterable<HTMLElement>,
-        );
+	useEffect(() => {
+		const headingElements = Array.from(
+			document.querySelectorAll(selector) as Iterable<HTMLElement>,
+		);
 
-        if (
-            headingElements.length > 1 &&
-            headingElements[0]?.tagName === "H1"
-        ) {
-            headingElements.shift();
-        }
+		if (
+			headingElements.length > 1 &&
+			headingElements[0]?.tagName === "H1"
+		) {
+			headingElements.shift();
+		}
 
-        const headings = headingElements.map(element => ({
-            title: element.innerText.replaceAll("&amp;", "&"),
-            id: element.id,
-        }));
+		const headings = headingElements.map(element => ({
+			title: element.innerText.replaceAll("&amp;", "&"),
+			id: element.id,
+		}));
 
-        setHeadings(headings);
-        setActiveId(headings[0].id);
-    }, [pathname]);
+		setHeadings(headings);
+		setActiveId(headings[0].id);
+	}, [pathname]);
 
-    useEffect(() => {
-        observer.current = new IntersectionObserver(
-            entries => {
-                for (const entry of entries) {
-                    if (entry?.isIntersecting) {
-                        setActiveId(entry.target.id);
-                    }
-                }
-            },
-            {
-                rootMargin: "-30% 0% -30% 0%",
-            },
-        );
+	useEffect(() => {
+		observer.current = new IntersectionObserver(
+			entries => {
+				for (const entry of entries) {
+					if (entry?.isIntersecting) {
+						setActiveId(entry.target.id);
+					}
+				}
+			},
+			{
+				rootMargin: "-30% 0% -70% 0%",
+			},
+		);
 
-        const elements = document.querySelectorAll(selector);
-        elements.forEach(element => observer.current?.observe(element));
+		const elements = document.querySelectorAll(selector);
+		elements.forEach(element => observer.current?.observe(element));
 
-        return () => {
-            observer.current?.disconnect();
-            setActiveId("");
-        };
-    }, [pathname]);
+		return () => {
+			observer.current?.disconnect();
+			setActiveId("");
+		};
+	}, [pathname]);
 
-    const onlyOne = headings.length === 1;
+	const onlyOne = headings.length === 1;
 
-    return (
-        <Root>
-            <h4 className="pl-[15px] mb-3 mt-4 uppercase font-bold tracking-wider text-[15px]">
-                On this page
-            </h4>
-            <ul className="list-none pr-2.5">
-                {headings.map(heading => (
-                    <li key={heading.id}>
-                        <a
-                            className={`my-2 block pl-[15px] ${
-                                activeId === heading.id || onlyOne
-                                    ? "text-blue-500 after:[content:'●'] after:ml-2 after:inline-block after:text-blue-500"
-                                    : "hover:text-blue-500"
-                            }`}
-                            href={`#${heading.id}`}
-                            onClick={event => {
-                                event.preventDefault();
+	return (
+		<Root>
+			<h4 className="pl-[15px] mb-3 mt-4 uppercase font-bold tracking-wider text-[15px]">
+				On this page
+			</h4>
+			<ul className="list-none pr-2.5">
+				{headings.map(heading => (
+					<li key={heading.id}>
+						<a
+							className={`my-2 block pl-[15px] ${
+								activeId === heading.id || onlyOne
+									? "text-blue-500 after:[content:'●'] after:ml-2 after:inline-block after:text-blue-500"
+									: "hover:text-blue-500"
+							}`}
+							href={`#${heading.id}`}
+							onClick={event => {
+								event.preventDefault();
 
-                                const element = document.getElementById(
-                                    heading.id,
-                                );
+								const element = document.getElementById(
+									heading.id,
+								);
+								const autolink = element?.querySelector(
+									"a.autolink",
+								) as HTMLAnchorElement | undefined;
 
-                                element?.scrollIntoView({
-                                    behavior: "smooth",
-                                    block: "center",
-                                    inline: "center",
-                                });
-                            }}
-                        >
-                            {heading.title}
-                        </a>
-                    </li>
-                ))}
-            </ul>
-        </Root>
-    );
+								if (autolink) {
+									autolink.click();
+								} else {
+									element?.scrollIntoView({
+										behavior: "smooth",
+										block: "center",
+										inline: "center",
+									});
+								}
+							}}
+						>
+							{heading.title}
+						</a>
+					</li>
+				))}
+			</ul>
+		</Root>
+	);
 }
